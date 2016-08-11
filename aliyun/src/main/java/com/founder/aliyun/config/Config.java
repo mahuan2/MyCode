658a81955b7ced3d7eb2bc3d8ec127789e254715
mahuan <523691298@qq.com>
2016-08-11 10:06:11 +0800

    init

--- aliyun/src/main/java/com/founder/aliyun/config/Config.java
+++ aliyun/src/main/java/com/founder/aliyun/config/Config.java
@@ -1,7 +1,7 @@
 package com.founder.aliyun.config;
 
 public class Config {
-	public static final String key = "5tveA0IHKZ1OWTWx";
-	public static final String secret = "aLRczNbBn9HINu4sje9K79YPjeBvQC";
+	public static final String key = "123";
+	public static final String secret = "123";
 	public static final String url = "https://shujuapi.aliyun.com/dataplus_124469/prophet/";
 }
